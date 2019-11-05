047de9b48793e2e4e4f3b1f4f9526c35ed098f58
Jarid <jarid.siewierski@gmail.com>
2019-11-05 16:37:32 -0700

    Trying to display predictions in a new page in the flask app.

--- app/app.py
+++ app/app.py
@@ -2,14 +2,34 @@ from flask import Flask, request, render_template
 import json
 import requests
 import socket
+import pickle
 
 app = Flask(__name__)
 PORT = 8080
 
 
-@app.route('/')
+with open('../models/naive_bayes_companies.pkl', 'rb') as f:
+    clf_company = pickle.load(f)
+
+
+with open('../models/naive_bayes_sentiment.pkl', 'rb') as f:
+    clf_sentiment = pickle.load(f)
+
+
+@app.route('/', methods=['GET'])
 def root():
     return render_template('home.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    """Recieve the article to be classified from an input form and use the
+    model to classify.
+    """
+    data = str(request.form['tweet_body'])
+    pred_company = str(clf_company.predict([[data]]))
+    pred_sent = str(clf_sent.predict([[data]]))
+    return render_template('predict.html', tweet=data, pred_company=pred_company, pred_sent=pred_sent)
 
 
 if __name__ == '__main__':
